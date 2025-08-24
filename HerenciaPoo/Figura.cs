a4fff7ddc19b67450e53eb6267165c384b08bd02
geraldinecastano064 <geraldine.castano064@pascualbravo.edu.co>
2025-08-24 16:37:31 -0500

    Agregar archivos de proyecto.

---
+++ HerenciaPoo/Figura.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaPoo
+{
+    internal class Figura
+    {
+        public class Figure
+        {
+            #region
+            public int positionX;
+            public int positionY;
+
+
+            public double CalculateArea()
+            {
+                return 0;
+            }
+            public double CalculatePerimeter()
+            {
+                return 0;
+            }
+            #endregion
+        }
+        public class Circule : Figure
+        {
+            private double radio;
+
+            public Circule(double radio)
+            {
+                this.radio = radio;
+
+            }
+            public double CalculateArea()
+            {
+                double area = Math.PI * radio * radio;
+                return area;
+            }
+            public double CalculatePerimeter()
+            {
+                double perimeter = 2 * Math.PI * radio;
+                return perimeter;
+            }
+        }
+        public class Triangle : Figure
+        {
+            private double basesTriangle;
+            private double heightTriangle;
+            private double side1,side2,side3;
+           
+            public Triangle(double basesTriangle, double heightTriangle, double side1, double side2, double side3)
+            {
+                this.basesTriangle = basesTriangle;
+                this.heightTriangle = heightTriangle;
+                this.side1 = side1;
+                this.side2 = side2;
+                this.side3 = side3;
+
+            }
+            public double CalculateArea()
+            {
+                double area = (basesTriangle * heightTriangle) / 2;
+                return area;
+            }
+            public double CalculatePerimeter()
+            {
+                double perimeter = side1+side2+side3;
+                return perimeter;
+            }
+        }
+        public class Rectangle : Figure
+        {
+            private double basesRectangle;
+            private double heightRectangle;
+
+            public Rectangle(double basesRectangle, double heightRectangle)
+            {
+                this.basesRectangle = basesRectangle;
+                this.heightRectangle = heightRectangle;
+            }
+            public double CalculateArea()
+            {
+                double area = basesRectangle * heightRectangle;
+                return area;
+            }
+            public double CalculatePerimeter()
+            {
+                double perimeter = 2*(basesRectangle+heightRectangle);
+                return perimeter;
+            }
+        }
+        
+    }
+}
---
+++ HerenciaPoo/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using static HerenciaPoo.Figura;
+
+while (true)
+{
+    Console.WriteLine("¿Que figura deseas cálcular?");
+    Console.WriteLine("1.Circulo");
+    Console.WriteLine("2.Triangulo");
+    Console.WriteLine("3.Rectangulo");
+    Console.WriteLine("4.Salir");
+    int opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingresa el radio del circulo:");
+            double radio = double.Parse(Console.ReadLine());
+
+            Circule circulo = new Circule(radio);
+
+            Console.WriteLine("El Área del circulo es: " + circulo.CalculateArea());
+            Console.WriteLine("El Perímetro del circulo es: " + circulo.CalculatePerimeter() + Environment.NewLine);
+
+            break;
+        case 2:
+            Console.WriteLine("Ingresa la base del triangulo:");
+            double basesTriangle = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa la altura del triangulo:");
+            double heightTriangle = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el lado 1 del triangulo:");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el lado 2 del triangulo:");
+            double side2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el lado 3 del triangulo:");
+            double side3 = double.Parse(Console.ReadLine());
+
+            Triangle triangulo = new Triangle(basesTriangle, heightTriangle, side1, side2, side3);
+
+            Console.WriteLine("El Área del circulo es: " + triangulo.CalculateArea());
+            Console.WriteLine("El Perímetro del circulo es: " + triangulo.CalculatePerimeter() + Environment.NewLine);
+            break;
+        case 3:
+            Console.WriteLine("Ingresa la base del Rectangulo:");
+            double basesRectangle = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa la altura del Rectangulo:");
+            double heightRectangle = double.Parse(Console.ReadLine());
+
+            Rectangle rectangulo = new Rectangle(basesRectangle, heightRectangle);
+            Console.WriteLine("El Área del circulo es: " + rectangulo.CalculateArea());
+            Console.WriteLine("El Perímetro del circulo es: " + rectangulo.CalculatePerimeter() + Environment.NewLine);
+            break;
+        case 4:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opción incorrecta" + Environment.NewLine);
+            break;
+
+    }
+
+
+
+}
